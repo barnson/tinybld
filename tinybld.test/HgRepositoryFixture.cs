5dcbb80325e1ec327e7890d95a4c74c63743df7d
Rob Mensching <rob@robmensching.com>
2013-02-26 18:13:20 -0800

    Implement hg repository.

---
+++ tinybld.test/HgRepositoryFixture.cs
@@ -0,0 +1,187 @@
+﻿namespace RobMensching.TinyBuild.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using RobMensching.TinyBuild.Tests.Helpers;
+    using Xunit;
+
+    public class HgRepositoryFixture : BaseFixture
+    {
+        private static readonly string RemoteRepository = @"Resources\hgtestrepo";
+        private static readonly string LocalTestProxyRepository;
+        private static readonly string LocalRepository;
+
+        static HgRepositoryFixture()
+        {
+            LocalTestProxyRepository = Path.Combine(Path.GetTempPath(), "tinybld_hgtest_proxy");
+            LocalRepository = Path.Combine(Path.GetTempPath(), "tinybld_hgtest");
+        }
+
+        [Fact]
+        public void CanDetectAbsentRepository()
+        {
+            var repo = CreateTestHgManager();
+
+            Assert.Equal(RepositoryStatus.Absent, repo.Check());
+            Assert.Equal(DateTime.MinValue, repo.LastUpdated);
+            Assert.False(Directory.Exists(repo.LocalRepositoryPath));
+        }
+
+        [Fact]
+        public void CanDetectEmptyRepository()
+        {
+            var repo = CreateTestHgManager();
+            Directory.CreateDirectory(repo.LocalRepositoryPath);
+
+            Assert.Equal(RepositoryStatus.Absent, repo.Check());
+            Assert.Equal(DateTime.MinValue, repo.LastUpdated);
+            Assert.True(Directory.Exists(repo.LocalRepositoryPath));
+        }
+
+        [Fact]
+        public void CanCreateAbsentRepository()
+        {
+            var repo = CreateTestHgManager();
+            Assert.True(repo.Update());
+            Assert.True(DateTime.MinValue < repo.LastUpdated);
+            Assert.True(Directory.Exists(repo.LocalRepositoryPath));
+        }
+
+        [Fact]
+        public void CanDetectRepositoryStable()
+        {
+            var repo = CreateTestHgManager();
+            Assert.True(repo.Update());
+            DateTime updatedAt = repo.LastUpdated;
+
+            Assert.Equal(RepositoryStatus.UpToDate, repo.Check());
+            Assert.True(DateTime.MinValue < repo.LastUpdated);
+            Assert.False(repo.Update());
+            Assert.Equal(updatedAt, repo.LastUpdated);
+        }
+
+        [Fact]
+        public void CanDetectRepositoryChanged()
+        {
+            var repo = CreateProxiedTestHgManager();
+            repo.Update();
+
+            using (var file = File.CreateText(Path.Combine(HgRepositoryFixture.LocalTestProxyRepository, "added.txt")))
+            {
+                file.WriteLine("This is added.txt");
+            }
+
+            var addCmd = new ProcessManager("hg", "add added.txt", HgRepositoryFixture.LocalTestProxyRepository).Run();
+            Assert.Equal(0, addCmd.ExitCode);
+            var commitCmd = new ProcessManager("hg", "commit -m added.txt", HgRepositoryFixture.LocalTestProxyRepository).Run();
+            Assert.Equal(0, commitCmd.ExitCode);
+
+            Assert.Equal(RepositoryStatus.OutOfDate, repo.Check());
+            Assert.Equal(RepositoryStatus.OutOfDate, repo.Check()); // ensure the check doesn't make the repo up to date.
+        }
+
+        [Fact]
+        public void CanUpdateChangedRepository()
+        {
+            var repo = CreateProxiedTestHgManager();
+            repo.Update();
+            var updatedAt = repo.LastUpdated;
+
+            using (var file = File.CreateText(Path.Combine(HgRepositoryFixture.LocalTestProxyRepository, "added.txt")))
+            {
+                file.WriteLine("This is added.txt");
+            }
+
+            var addCmd = new ProcessManager("hg", "add added.txt", HgRepositoryFixture.LocalTestProxyRepository).Run();
+            Assert.Equal(0, addCmd.ExitCode);
+            var commitCmd = new ProcessManager("hg", "commit -m added.txt", HgRepositoryFixture.LocalTestProxyRepository).Run();
+            Assert.Equal(0, commitCmd.ExitCode);
+
+            Assert.Equal(RepositoryStatus.OutOfDate, repo.Check());
+            Assert.True(repo.Update());
+            Assert.Equal(RepositoryStatus.UpToDate, repo.Check());
+            Assert.True(updatedAt < repo.LastUpdated);
+        }
+
+        [Fact]
+        public void CanGetChangesFromUpdatedRepository()
+        {
+            var repo = CreateProxiedTestHgManager();
+            repo.Update();
+            var updatedAt = repo.LastUpdated;
+
+            using (var file = File.CreateText(Path.Combine(HgRepositoryFixture.LocalTestProxyRepository, "added.txt")))
+            {
+                file.WriteLine("This is added.txt");
+            }
+
+            var addCmd = new ProcessManager("hg", "add added.txt", HgRepositoryFixture.LocalTestProxyRepository).Run();
+            Assert.Equal(0, addCmd.ExitCode);
+            var commitCmd = new ProcessManager("hg", "commit -m \"commit added.txt\"", HgRepositoryFixture.LocalTestProxyRepository).Run();
+            Assert.Equal(0, commitCmd.ExitCode);
+
+            RepositoryChange[] changes = repo.Changes();
+            Assert.Equal(1, changes.Length);
+            Assert.Equal("Added a.txt in original hgtestrepo.", changes[0].Message);
+
+            Assert.True(repo.Update());
+
+            changes = repo.Changes();
+            Assert.Equal(2, changes.Length);
+            Assert.False(String.IsNullOrEmpty(changes[0].Id));
+            Assert.False(String.IsNullOrEmpty(changes[0].Author));
+            Assert.Equal("commit added.txt", changes[0].Message);
+            Assert.False(String.IsNullOrEmpty(changes[1].Id));
+            Assert.False(String.IsNullOrEmpty(changes[1].Author));
+            Assert.Equal("Added a.txt in original hgtestrepo.", changes[1].Message);
+            Assert.True(changes[0].Date > changes[1].Date);
+
+            changes = repo.Changes(changes[1].Id);
+            Assert.Equal(1, changes.Length);
+            Assert.False(String.IsNullOrEmpty(changes[0].Id));
+            Assert.False(String.IsNullOrEmpty(changes[0].Author));
+            Assert.Equal("commit added.txt", changes[0].Message);
+
+            changes = repo.Changes(changes[0].Id);
+            Assert.Equal(0, changes.Length);
+        }
+
+        private HgRepository CreateTestHgManager(string branch = null)
+        {
+            DeleteDirectory(HgRepositoryFixture.LocalRepository);
+            RegisterForCleanup(HgRepositoryFixture.LocalRepository);
+
+            return new HgRepository()
+            {
+                Branch = branch,
+                LocalRepositoryPath = HgRepositoryFixture.LocalRepository,
+                RemoteRepositoryPath = HgRepositoryFixture.RemoteRepository,
+            };
+        }
+
+        private HgRepository CreateProxiedTestHgManager(string branch = null)
+        {
+            DeleteDirectory(HgRepositoryFixture.LocalTestProxyRepository);
+            DeleteDirectory(HgRepositoryFixture.LocalRepository);
+            RegisterForCleanup(HgRepositoryFixture.LocalTestProxyRepository);
+            RegisterForCleanup(HgRepositoryFixture.LocalRepository);
+
+            var testRepo = new HgRepository()
+            {
+                Branch = branch,
+                LocalRepositoryPath = HgRepositoryFixture.LocalTestProxyRepository,
+                RemoteRepositoryPath = HgRepositoryFixture.RemoteRepository,
+            }.Update();
+
+            return new HgRepository()
+            {
+                Branch = branch,
+                LocalRepositoryPath = HgRepositoryFixture.LocalRepository,
+                RemoteRepositoryPath = HgRepositoryFixture.LocalTestProxyRepository,
+            };
+        }
+    }
+}
--- tinybld/HgRepository.cs
+++ tinybld/HgRepository.cs
@@ -1,6 +1,14 @@
 ﻿namespace RobMensching.TinyBuild
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public enum HgReturnCode
+    {
+        Success = 0,
+        NoRepository = 255,
+    };
 
     public class HgRepository : IRepository
     {
@@ -14,22 +22,152 @@
 
         public RepositoryChange[] Changes(string since = null, string until = null, string filename = null)
         {
-            throw new NotImplementedException();
+            string revset = null;
+            if (!String.IsNullOrEmpty(since) || !String.IsNullOrEmpty(until))
+            {
+                revset = String.Join(String.Empty,
+                                     "--rev (", since, "..", until, ")",
+                                     String.IsNullOrEmpty(since) ? null : "-" + since,
+                                     String.IsNullOrEmpty(until) ? null : "-" + until);
+            }
+
+            var logArgs = new[]
+                        {
+                            "log --template \"changeset: {node}\nuser: {author}\ndate: {date|rfc3339date}\ndescription: \n{desc}\n\"",
+                            revset,
+                            filename,
+                        };
+            ProcessManager logCmd = new ProcessManager("hg", logArgs, this.LocalRepositoryPath).Run();
+            if (logCmd.ExitCode != (int)GitReturnCode.Success)
+            {
+                throw new ApplicationException(String.Format("hg exit code: {0}\r\nstdout:{1}\r\nstderr:{2}", logCmd.ExitCode, logCmd.StandardOutput, logCmd.StandardError));
+            }
+
+            List<RepositoryChange> changes = new List<RepositoryChange>();
+            RepositoryChange change = null;
+            bool description = false;
+            foreach (var line in logCmd.StandardOutput.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (line.StartsWith("changeset:"))
+                {
+                    description = false;
+
+                    change = new RepositoryChange()
+                    {
+                        Id = line.Substring(10).Trim(),
+                    };
+
+                    changes.Add(change);
+                }
+                else if (line.StartsWith("user:"))
+                {
+                    change.Author = line.Substring(6).Trim();
+                }
+                else if (line.StartsWith("date:"))
+                {
+                    change.Date = Convert.ToDateTime(line.Substring(6).Trim());
+                }
+                else if (line.StartsWith("description:"))
+                {
+                    description = true;
+                }
+                else if (description)
+                {
+                    change.Message = change.Message + (String.IsNullOrEmpty(change.Message) ? String.Empty : "\r\n") + line;
+                }
+            }
+
+            return changes.ToArray();
         }
 
+        /// <summary>
+        /// Checks the current status of the local repository with
+        /// respect to the remote repository.
+        /// </summary>
+        /// <returns>Status of the local repository.</returns>
         public RepositoryStatus Check()
         {
-            throw new NotImplementedException();
+            if (!Directory.Exists(this.LocalRepositoryPath))
+            {
+                return RepositoryStatus.Absent;
+            }
+
+            ProcessManager incomingCmd = new ProcessManager("hg", "incoming", this.LocalRepositoryPath).Run();
+            if (incomingCmd.ExitCode == 0)
+            {
+                return RepositoryStatus.OutOfDate;
+            }
+            else if (incomingCmd.ExitCode == 1)
+            {
+                return RepositoryStatus.UpToDate;
+            }
+            else if (incomingCmd.ExitCode == (int)HgReturnCode.NoRepository)
+            {
+                return RepositoryStatus.Absent;
+            }
+
+            return RepositoryStatus.Error;
         }
 
         public void Clean()
         {
-            throw new NotImplementedException();
+            if (Directory.Exists(this.LocalRepositoryPath))
+            {
+                ProcessManager cleanCmd = new ProcessManager("hg", "clean --all", this.LocalRepositoryPath).Run();
+            }
         }
 
         public bool Update(bool rebase = false)
         {
-            throw new NotImplementedException();
+            ProcessManager pullCmd = null;
+            if (Directory.Exists(this.LocalRepositoryPath))
+            {
+                var pullArgs = new[]
+                    {
+                        "pull",
+                        "-u",
+                        rebase ? "--rebase" : null,
+                    };
+                pullCmd = new ProcessManager("hg", pullArgs, this.LocalRepositoryPath).Run();
+            }
+
+            // No repository, clone from the remote.
+            if (pullCmd == null || pullCmd.ExitCode == (int)GitReturnCode.NoRepository)
+            {
+                string[] cloneArgs = new[]
+                {
+                    "clone",
+                    "-q",
+                    String.IsNullOrEmpty(this.Branch) ? String.Empty : " -b " + this.Branch,
+                    this.RemoteRepositoryPath,
+                    this.LocalRepositoryPath,
+                };
+
+                ProcessManager cloneCmd = new ProcessManager("hg", cloneArgs).Run();
+                if (cloneCmd.ExitCode != (int)GitReturnCode.Success)
+                {
+                    throw new ApplicationException(String.Format("hg exit code: {0}\r\nstdout:{1}\r\nstderr:{2}", cloneCmd.ExitCode, cloneCmd.StandardOutput, cloneCmd.StandardError));
+                }
+
+                this.LastUpdated = DateTime.UtcNow;
+                return true;
+            }
+            else if (pullCmd.ExitCode == (int)GitReturnCode.Success)
+            {
+                if (String.IsNullOrEmpty(pullCmd.StandardError) &&
+                    (String.IsNullOrEmpty(pullCmd.StandardOutput) ||
+                     pullCmd.StandardOutput.TrimEnd().EndsWith("no changes found", StringComparison.OrdinalIgnoreCase)))
+                {
+                    return false;
+                }
+
+                this.LastUpdated = DateTime.UtcNow;
+                return true;
+            }
+            else
+            {
+                throw new ApplicationException(String.Format("hg exit code: {0}\r\nstdout:{1}\r\nstderr:{2}", pullCmd.ExitCode, pullCmd.StandardOutput, pullCmd.StandardError));
+            }
         }
     }
 }
